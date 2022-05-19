81bec39fa2c0c23bf5cd367bc11eaee6147c2c02
julian <juliangwillis@gmail.com>
2022-05-19 15:56:06 -0400

    Add function to create # edgeDivs from dimension

--- source.js
+++ source.js
@@ -5,27 +5,50 @@ import {
 } from './function.js'
 
 const container = document.getElementById('container');
-// create 256 (16*16)'etchDivs' :
-const etchDivArray = createSimilarElems('div', 256, ['etchDiv']);
-// add unique id's to the divs just because:
-for (let i = 0; i < etchDivArray.length; i++) {
-    etchDivArray[i].setAttribute('id', 'etchDiv' + i);
+const containerHeight = 1000; // 1000*1000 space for our etch-a-sketch
+let etchDivArray = [];
+
+// want each etchDiv to take up same amount of space in 1000*1000 container
+// all etchDivs are squares
+function calcDivHeight(dimension) {
+    return Math.floor(containerHeight / dimension);
 }
-// add content inside to see them better:
-etchDivArray.forEach(etchDiv => {
-    etchDiv.innerText = 'hello div'
-});
 
-appendSiblings(container, etchDivArray); // put etchDivs in container
-
-// event listener for mouseover to change bg:
-for (let i = 0; i < etchDivArray.length; i++) {
-    etchDivArray[i].addEventListener('mouseover', () => {
-        etchDivArray[i].style.backgroundColor = 'green';
+function createEtchDivs(dimension) {
+    // calculate how many divs total we will make
+    let totalNumDiv = dimension * dimension; 
+    // calculate how large we want each div
+    let divHeight = calcDivHeight(dimension); 
+    // redo array to be new divs
+    etchDivArray = createSimilarElems('div', totalNumDiv, ['etchDiv']);
+    // add unique id's to the divs just because:
+    for (let i = 0; i < totalNumDiv; i++) {
+        etchDivArray[i].setAttribute('id', 'etchDiv' + i);
+    }
+    // set size from calculated divHeight, and inner content:
+    etchDivArray.forEach(etchDiv => {
+        etchDiv.innerText = 'hello div' // remove this later
+        etchDiv.style.height = divHeight; // variable
+        etchDiv.style.width = divHeight; // variable
     });
+    // event listener for mouseover to change bg:
+    for (let i = 0; i < totalNumDiv; i++) {
+        etchDivArray[i].addEventListener('mouseover', () => {
+            etchDivArray[i].style.backgroundColor = 'green';
+        });
+    }
+    console.log(etchDivArray);
+    // put on page
+    appendSiblings(container, etchDivArray);
+
 }
 
-const height = (1000);
-let totalRows = 16;
-let calculatedHeight = Math.floor(height / totalRows);
-console.log(calculatedHeight);
+// Init:
+createEtchDivs(16);
+
+const btn = document.getElementById('btn');
+btn.addEventListener('click', () => {
+    etchDivArray.forEach(etchDiv => {
+        etchDiv.remove();
+    });    
+})
