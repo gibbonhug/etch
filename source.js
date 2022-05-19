1251fdadf1a1c6b989a00e0f5cf004e16ea3cf03
julian <juliangwillis@gmail.com>
2022-05-19 16:07:04 -0400

    Add addEdgeDivListeners function and reorganize code

--- source.js
+++ source.js
@@ -12,44 +12,50 @@ const container = document.getElementById('container');
 const containerHeight = 1000; // 1000*1000 space for our etch-a-sketch
 let etchDivArray = [];
 
+// divs are arranged in a square
+function calcDivNumber(dimension) {
+    return (dimension*dimension);
+}
+
 // want each etchDiv to take up same amount of space in 1000*1000 container
 // all etchDivs are squares
 function calcDivHeight(dimension) {
     return Math.floor(containerHeight / dimension);
 }
 
-// divs are arranged in a square
-function calcDivNumber(dimension) {
-    return (dimension*dimension);
-}
-
 function createEtchDivs(dimension) {
+    // remove old divs
+    removeEdgeDivs();
     // calculate how many divs total we will make
     let totalNumDiv = calcDivNumber(dimension); 
     // calculate how large we want each div
     let divHeight = calcDivHeight(dimension); 
-    // redo array to be new divs
+    // reinit array to be new divs
+    etchDivArray = [];
     etchDivArray = createSimilarElems('div', totalNumDiv, ['etchDiv']);
     // add unique id's to the divs just because:
     for (let i = 0; i < totalNumDiv; i++) {
         etchDivArray[i].setAttribute('id', 'etchDiv' + i);
     }
-    // set size from calculated divHeight, and inner content:
+    // set size from calculated divHeight, and add text content:
     etchDivArray.forEach(etchDiv => {
-        etchDiv.innerText = 'hello div' // remove this later
         etchDiv.style.height = divHeight; // variable
         etchDiv.style.width = divHeight; // variable
+        etchDiv.innerText = 'hello div' // remove this later
     });
-    // event listener for mouseover to change bg:
-    for (let i = 0; i < totalNumDiv; i++) {
-        etchDivArray[i].addEventListener('mouseover', () => {
-            etchDivArray[i].style.backgroundColor = 'green';
-        });
-    }
-    console.log(etchDivArray);
+    // event listener for mouseover to change bg to green for now:
+    addEdgeDivListeners(totalNumDiv, 'green');
     // put on page
     appendSiblings(container, etchDivArray);
 
+}
+
+function addEdgeDivListeners(numDiv, bgColor) {
+    for (let i = 0; i < numDiv; i++) {
+        etchDivArray[i].addEventListener('mouseover', () => {
+            etchDivArray[i].style.backgroundColor = bgColor;
+        });
+    }
 }
 
 function removeEdgeDivs() {
